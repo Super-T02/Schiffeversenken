326ee28f375b015a95bd5d52a76165cfbbe7d531
Super-T02 <Super-T02@users.noreply.github.com>
2021-02-02 16:00:21 +0100

    Added Player Arrays
    Added both arrays and filed them with 0's
    
    Co-Authored-By: mxm381 <78032612+mxm381@users.noreply.github.com>

--- Files/Global.h
+++ Files/Global.h
@@ -1,1 +1,15 @@
 #include <stdio.h>
+#define ARRAYSIZE 12
+
+enum FELDNUMBER { A = 0, B, C, D, E, F, G, H, I, J, K, L };
+enum SCHIFFSTATUS {neutral = 0, ship, fail, hit, destroyed};
+
+//Array defenition
+
+//Player A
+int playerA[ARRAYSIZE][ARRAYSIZE];
+
+//Player B
+int playerB[ARRAYSIZE][ARRAYSIZE];
+
+	
--- Files/Main.c
+++ Files/Main.c
@@ -2,5 +2,16 @@
 
 int main()
 {
+	//Arrays
+	for (size_t i = 0; i < ARRAYSIZE; i++)
+	{
+		for (size_t j = 0; j < ARRAYSIZE; j++)
+		{
+			playerA[i][j] = neutral;
+			playerB[i][j] = neutral;
+		}
+	}
+
+
 	printf("Hello World!!!");
 }
