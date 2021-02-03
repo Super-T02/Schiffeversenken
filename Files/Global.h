6213d19e15c8727dc4274d8452aead3e48ca3763
Super-T02 <Super-T02@users.noreply.github.com>
2021-02-03 09:20:47 +0100

    Comments
    added a few comments

--- Files/Global.h
+++ Files/Global.h
@@ -1,21 +1,21 @@
+//Bibs
 #include <stdio.h>
+
+//Constants
 #define ARRAYSIZE 12
 
+//Enums
 enum FELDNUMBER { A = 0, B, C, D, E, F, G, H, I, J, K, L };
 enum SCHIFFSTATUS {neutral = 0, ship, fail, hit, destroyed};
 
 //Array defenition
+	//Player A
+	int playerA[ARRAYSIZE][ARRAYSIZE];
 
-//Player A
-int playerA[ARRAYSIZE][ARRAYSIZE];
-
-//Player B
-int playerB[ARRAYSIZE][ARRAYSIZE];
+	//Player B
+	int playerB[ARRAYSIZE][ARRAYSIZE];
 
 	
-
-
-
 
 //Header
 #include "End.h"
