defaeca5e36b87004336c82c1a421c54a0b81211
Super-T02 <Super-T02@users.noreply.github.com>
2021-02-17 10:43:04 +0100

    Added Makefile and debugging information

---
+++ cmake-build-debug/CMakeFiles/Schiffeversenken.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/tomfr/CLionProjects/Schiffeversenken/Files/End.c" "C:/Users/tomfr/CLionProjects/Schiffeversenken/cmake-build-debug/CMakeFiles/Schiffeversenken.dir/Files/End.c.obj"
+  "C:/Users/tomfr/CLionProjects/Schiffeversenken/Files/Main.c" "C:/Users/tomfr/CLionProjects/Schiffeversenken/cmake-build-debug/CMakeFiles/Schiffeversenken.dir/Files/Main.c.obj"
+  "C:/Users/tomfr/CLionProjects/Schiffeversenken/Files/Play.c" "C:/Users/tomfr/CLionProjects/Schiffeversenken/cmake-build-debug/CMakeFiles/Schiffeversenken.dir/Files/Play.c.obj"
+  "C:/Users/tomfr/CLionProjects/Schiffeversenken/Files/Start.c" "C:/Users/tomfr/CLionProjects/Schiffeversenken/cmake-build-debug/CMakeFiles/Schiffeversenken.dir/Files/Start.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Files"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Schiffeversenken.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Schiffeversenken.dir/Files/End.c.obj"
+  "CMakeFiles/Schiffeversenken.dir/Files/Main.c.obj"
+  "CMakeFiles/Schiffeversenken.dir/Files/Play.c.obj"
+  "CMakeFiles/Schiffeversenken.dir/Files/Start.c.obj"
+  "Schiffeversenken.exe"
+  "Schiffeversenken.exe.manifest"
+  "Schiffeversenken.lib"
+  "Schiffeversenken.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Schiffeversenken.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
